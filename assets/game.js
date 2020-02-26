204b11399ba3ce9655cd0b20f5c4e6821dae35f8
jadibrahim99 <jadintx@gmail.com>
2020-02-26 17:42:33 -0600

    html set up with divs containing the stats and instructions

--- assets/game.js
+++ assets/game.js
@@ -1,38 +1,15 @@
-// assign target number random
-
-// assign a random value to each crytal 1-12
-
-//use an onClick when user clicks each crystal
-
-//add the value of each click to the total score
-
-// if the totalScore equals randomNumber, user wins, and win is recorded
-
-// if totalScore is more than randomNumber, user loses, and loss is recorded
-
-// if win or loss, game resets and new numbers are assigned
-
-// assign variables
-$(document).ready(function() {
-
-var targetNumber;
-var crystal1;
-var crystal2;
-var crystal3;
-var crystal4;
-var userTotal;
-var wins = 0;
-var losses = 0;
-
-
-// reset function
-
-function reset (){
-    crystal1 = Math.floor(Math.random() * 11 + 1);
-    crystal2 = Math.floor(Math.random() * 11 + 1);
-    crystal3 = Math.floor(Math.random() * 11 + 1);
-    crystal4 = Math.floor(Math.random() * 11 + 1);
-    
-}
-
-}
+const CrystalCollector = function() {
+        /************************************************************************
+            
+            Private variables
+            
+        *************************************************************************/
+        // Variables for the game
+        
+        
+        const numCrystals   = 4;
+        let   crystalValues = new Array(numCrystals);
+        
+        // Variables for the user
+        let numWins = 0, numLosses = 0;
+        let targetSum, currentSum;
